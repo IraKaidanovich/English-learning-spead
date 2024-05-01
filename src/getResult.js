744758baa010d0497d5a0730bd15af72c0682a45
Iryna Kaidanovych <ykyway18@gmail.com>
2024-05-01 19:34:24 +0200

    Calculate understanding of the book before reading it, rather than after reading it

--- src/getResult.js
+++ src/getResult.js
@@ -17,45 +17,46 @@ module.exports = function (allWords) {
     'will understand': [],
     'will understand per book': [],
   }
-  for(let i = 0; i < books.length; i++) {
-    const words = books.slice(0, i + 1).flat(); // each time the amount of words is bigger and bigger
-    const map = mapWords(words);
+
+  let lastBookLearnedWords = [];
+  let lastBookRecognizableWords = [];
+  for(let bookIndex = 0; bookIndex < books.length; bookIndex++) {
+    const seenWords = books.slice(0, bookIndex + 1).flat();
+    const map = mapWords(seenWords);
     const distinctWords = filterDistinctWords(map);
     const wordsCategories = categorizeWords(distinctWords);
 
-    const bookWords = books[i];
+    const bookWords = books[bookIndex];
     const bookWordsMap = mapWords(bookWords);
     const bookDistinctWordsList = filterDistinctWords(bookWordsMap);
     const bookProperWordsCount = bookDistinctWordsList.reduce(([wordA, countA], [wordB, countB]) => ['', countA + countB], ['', 0])[1];
 
     result['all words'].push(distinctWords.length); 
-  
-    let willLearnWords;
-    let willRecognizeWords;
-    wordsCategories.forEach(([categoryName, categoryDistinctList, categoryTotalCount]) => {
-      if(!result[categoryName]) result[categoryName] = [];
-
-      result[categoryName].push(categoryDistinctList.length);
-
-      if(i + 1 === books.length) saveWords(categoryName, categoryDistinctList);
-
-      if(categoryName === 'will learn') willLearnWords = categoryDistinctList;
-      if(categoryName === 'will recognize') willRecognizeWords = categoryDistinctList;
-    });
 
     const [understandingRate, understandingRatePerBook] = calculateUnderstanding({
-      learnedWords: willLearnWords, 
-      recognizableWords: willRecognizeWords, 
+      learnedWords: lastBookLearnedWords, 
+      recognizableWords: lastBookRecognizableWords, 
       mapOfAllWords, 
       bookWordsMap,
       totalProperWordsCount,
       bookProperWordsCount
     });
 
+    wordsCategories.forEach(([categoryName, categoryDistinctList, categoryTotalCount]) => {
+      if(!result[categoryName]) result[categoryName] = [];
+
+      result[categoryName].push(categoryDistinctList.length);
+
+      if(bookIndex + 1 === books.length) saveWords(categoryName, categoryDistinctList);
+
+      if(categoryName === 'will learn') lastBookLearnedWords = categoryDistinctList;
+      if(categoryName === 'will recognize') lastBookRecognizableWords = categoryDistinctList;
+    });
+
     result['will understand'].push(understandingRate);
     result['will understand per book'].push(understandingRatePerBook);
 
-    console.log(`Book #${i + 1} out of ${books.length} finished`);
+    console.log(`Book #${bookIndex + 1} out of ${books.length} finished`);
   }
 
   return result;
